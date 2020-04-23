bd755dc704ded73edaf22945bf2c097f2fed8995
Prince <titipakorn.p@gmail.com>
2020-04-23 11:09:30 +0700

    update test classify

--- test_classify.py
+++ test_classify.py
@@ -1,14 +1,14 @@
 import requests
 import os
 import csv
-path = '/data/CIM/Test_API/'
+path = '/data1/CIM/Test_API/check/'
 
 with open('check_signature.csv', mode='w') as compare_result_file:
     csv_writer = csv.writer(
         compare_result_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for file in filter(os.listdir(path+'check/'), '*.[Pp][Dd][Ff]'):
+    for file in os.listdir(path):
         files = []
-        files.append(('file', open(path+'check/'+file, 'rb')))
+        files.append(('file', open(path+file, 'rb')))
         r = requests.post(
             'http://localhost:6666/check_signature/', files=files)
         data = r.json()
